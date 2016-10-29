5aeefd4a76f244422419111a37484967bfe4c28f
icunhacorrea <iago.cc@outlook.com>
2016-10-29 21:04:43 -0200

    corrigindo script

--- csv_handler.py
+++ csv_handler.py
@@ -1,41 +1,97 @@
 #!/usr/bin/python3
-
 import csv
 
-table = []; column = 2; summ = 0; count = 0; avgL = []; rCountry = []
+table = []; column = 2; summ = 0; count = 0; Country = []; rCountry = []
 
-with open('tabela.csv', 'r') as csvfile:
-	for line in csvfile:
-		table.append(line[:-1].split('","'))
+def read_csv(table):
+	with open('tabela.csv', 'r') as csvfile:
+		for line in csvfile:
+			table.append(line[:-1].split('","'))
 
-for column in range(2,14):
-	nullPoints = []
-	for i in range(2, len(table)):
+def write_csv(table):
+	with open('new_csv.csv', 'w', newline='') as csvfile:
+	    spamwriter = csv.writer(csvfile, delimiter=",", escapechar=";")
+	    for i in range(len(table)):
+	    	if i == 0:
+	    		spamwriter.writerow([""] + [""] + table[i][2:14])
+	    	elif i == 1:
+	    		spamwriter.writerow(["Country"] + table[i][1:14])
+	    	else:
+	    		spamwriter.writerow(table[i])
+
+def format_table(table):
+	for i in range(2,len(table)):
+		table[i][0] = table[i][0].replace('"', "")
+		table[i][1] = table[i][1].replace(" ", "")
 		table[i][13] = table[i][13].replace('"', "")
-		lNum = table[i][column].replace('"', "").split(" ")
-		if len(lNum) > 1:
-			aux = 0
-			for j in range(len(lNum)):
-				aux = aux + float(lNum[j])
-			avg = aux / (len(lNum))
-			summ = summ + avg
-			count = count + int(len(lNum))
-		elif lNum[0] != '':
-			summ = summ + float(lNum[0])
-			count = count + 1
-		elif lNum[0] == '':
-			nullPoints.append([i, column])
-	avg = summ/count; avgL.append(round(avg, 1))
-	for i in range(len(nullPoints)):
-		table[nullPoints[i][0]][nullPoints[i][1]] = round(avg, 1)
 
+def insert_number(n, table, nullPoints):
+	for i in range(len(nullPoints)):
+		table[nullPoints[i][0]][nullPoints[i][1]] = round(n, 1)
+
+def search_id(string):
+	for i in range(len(Country)):
+		if Country[i][0] == string:
+			return i
+
+read_csv(table)
+format_table(table)
+
+# Laço responsável por definir os intervalos de cada país
+i = 2
+while i < len(table):
+	atual_string = table[i][0];
+	aux = 1
+	id_ini = i
+	while True:
+		if i + aux > 853:
+			break
+		elif atual_string == table[i + aux][0]:
+			aux = aux + 1
+		else:
+			break
+	Country.append([atual_string, id_ini, aux - 1])
+	i = i + aux
+
+# Laço resposável por criar lista de países e suas respectivas médias por coluna
+for i in range(0, len(Country)):
+	id_base = Country[i][1]; id_lmit = Country[i][2]
+	del Country[i][1]; del Country[i][1]
+	for column in range(2, 14):
+		nullPoints = []
+		summ = 0; count = 0; j = 0
+		while j <= id_lmit:
+			lNum = table[id_base + j][column].split(" ")
+			if len(lNum) > 1:
+				aux = 0
+				for z in range(len(lNum)):
+					aux = aux + float(lNum[z])
+				avg = aux / len(lNum)
+				summ = summ + avg
+				count = count + len(lNum)
+			if lNum[0] != '':
+				summ = summ + float(lNum[0])
+				count = count + 1
+			elif lNum[0] == '':
+				nullPoints.append([id_base + j, column])
+			j = j + 1
+		if count == 0:
+			Country[i].append(0)
+			insert_number(0, table, nullPoints)
+			continue
+		else:
+			avg = summ / count
+			Country[i].append(round(avg, 1))
+			insert_number(avg, table, nullPoints)
+
+# Cria lista responsável pelos intervalos intra-país
 for i in range(2, 854):
-	table[i][0] = table[i][0].replace('"', "")
-	table[i][1] = table[i][1].replace(" ", "")
 	lPais = table[i][1].split("-")
 	if len(lPais) > 1:
-		rCountry.append([table[i][0], 1 + (int(lPais[1]) - int(lPais[0])), lPais[0], i])
+		id_country = search_id(table[i][0])
+		rCountry.append([table[i][0], 1 + (int(lPais[1]) - int(lPais[0])), lPais[0], i, id_country])
 
+# Remove da lista principal as tuplas com anos intervalados.
 count = 0
 for i in range(len(rCountry)):
 	del table[rCountry[i][3]]
@@ -45,16 +101,8 @@ for i in range(len(rCountry)):
 
 for i in range(len(rCountry)):
 	for j in range(rCountry[i][1]):
-		table.append([rCountry[i][0], int(rCountry[i][2]) + j,avgL[0], avgL[1], avgL[2], avgL[3], avgL[4], avgL[5], avgL[6], avgL[7], avgL[8], avgL[9], avgL[10], avgL[11]])
+		idC = rCountry[i][4]
+		table.append([Country[idC][0], int(rCountry[i][2]) + j, Country[idC][1], Country[idC][2], Country[idC][3], Country[idC][4], Country[idC][6], Country[idC][6], Country[idC][7], Country[idC][8], Country[idC][9], Country[idC][10], Country[idC][11], Country[idC][12]])
 
 table.sort(key=lambda x : x[0])
-
-with open('new_csv.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=",", escapechar=";")
-    for i in range(len(table)):
-    	if i == 0:
-    		spamwriter.writerow([""] + [""] + table[i][2:14])
-    	elif i == 1:
-    		spamwriter.writerow(["Country"] + table[i][1:14])
-    	else:
-    		spamwriter.writerow(table[i])
+write_csv(table)
